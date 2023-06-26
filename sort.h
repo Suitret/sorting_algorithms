c585cd8006bdd2397b88a3e0b60ffe021f5c0bf9
Suitret <suitret01@gmail.com>
2023-06-26 14:14:46 -0700

    sort.h

---
+++ sort.h
@@ -0,0 +1,41 @@
+#ifndef _SORT_H
+#define _SORT_H
+
+#include <stddef.h> /* Because of 'size_t' */
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void quick_sort_(int *array, int beg, int end);
+int partition(int a[], int beg, int end);
+void swap_and_print(listint_t *tmp, listint_t **list, listint_t *ptr);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void heapify(int *array, size_t size, int end, int i);
+void swap(int *a, int *b);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+
+#endif /* _SORT_H */
