3818af3eff467a5054b869aed1b0feca2b890d98
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-06-27 20:01:29 +0100

    modified shell sort algo

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,21 +9,32 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	int gap, temp, j;
-	size_t i;
+	size_t gap = 1, i, j;
+	int temp;
 
-	if (size > 1 && array)
+	if (!array || size < 2)
+		return;
+
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap > 0)
 	{
-		for (gap = size / 2; gap > 0; gap /= 2)
+		for (i = gap; i < size; i++)
 		{
-			for (i = gap; i < size; i += 1)
+			temp = array[i];
+			j = i;
+
+			while (j >= gap && array[j - gap] > temp)
 			{
-				temp = array[i];
-				for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-					array[j] = array[j - gap];
-				array[j] = temp;
+				array[j] = array[j - gap];
+				j -= gap;
 			}
-			print_array(array, size);
+
+			array[j] = temp;
 		}
+
+		print_array(array, size);
+		gap /= 3;
 	}
 }
